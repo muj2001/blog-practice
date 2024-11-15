0d99e2e700db781f1bfab4e54c3d07f704983094
usman-jehangir-devsinc <usman.jehangir@devsinc.com>
2024-11-15 18:18:31 +0500

    Added Some Category functionality, and removed User Mailer

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
 
-  belongs_to :user, -> { includes :sections }
+  belongs_to :user
   has_and_belongs_to_many :categories
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :sections, dependent: :destroy
