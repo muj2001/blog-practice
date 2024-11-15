dd14adc22ed115e190622dfb73680b17ac753a69
usman-jehangir-devsinc <usman.jehangir@devsinc.com>
2024-11-15 18:45:09 +0500

    Added User Mailer, and configured it, added a test method to check whether it is functional

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,17 +36,17 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  # config.action_mailer.smtp_settings = {
-  #   address:              "smtp.gmail.com",
-  #   port:                 587,
-  #   domain:               "localhost",
-  #   user_name:            APP_PASSWORD,
-  #   password:             EMAIL,
-  #   authentication:       "plain",
-  #   enable_starttls_auto: true,
-  #   open_timeout:         5,
-  #   read_timeout:         5
-  # }
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "localhost",
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_APP_PASSWORD"],
+    authentication:       "plain",
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5
+  }
   # Don't care if the mailer can't send.
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
